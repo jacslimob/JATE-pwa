9f118e6954f94cfa484fe665d256a883c8f8d7cd
Jacob Maughan <135153003+jacslimob@users.noreply.github.com>
2023-11-13 01:53:11 -0700

    updated webpack.config.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Text Editor Starter Code
+in client/package.json may need to try deleting deprecated packages
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template:'./index.html',
+        title: 'Jate'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Take notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        // screenshots: [
+        //   {
+        //     src: path.resolve('src/images/screenshot.png'),
+        //     sizes: 'any',
+        //     form_factor: "wide",
+        //     label: "Homescreen of J.A.T.E App",
+        //     destination: path.join('assets', 'screenshots'),
+        //   },
+        // ],
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          
+          use: {
+            loader: 'babel-loader',
+            options: {
+              // need to look up what this does
+              presets: ['@babel/preset-env'],
+              // may not need object-rest-spread
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
